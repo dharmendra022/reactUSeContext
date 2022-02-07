9b6a0417491aa0f18d3b5d3f85e36caa7ca42d90
Dharmendra Singh Yadav <dharmendrasinghyadav022@gmail.com>
2022-02-07 09:12:53 +0530

    commit

---
+++ src/component/useCotext/ComB.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import ComC from "./ComC"
+
+const ComB = ({name}) => {
+  return (
+      <>
+          <h1>ComB g{name}</h1>
+          {/* <h1>Dharmendra Singh Yadav</h1> */}
+          <ComC/>
+      </>
+  )
+};
+
+export default ComB;
+
+// context
+// provider
+// consumer
+
+// useContext
+
+// useContext
+// contet Api/useContext
